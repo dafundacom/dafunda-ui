6e059f4247420dff56866bba266765a1f2ac8447
karyanayandi <halo@yandi.me>
2023-07-06 12:10:19 +0700

    fix(drop-zone): placeholder

---
+++ .changeset/violet-meals-sin.md
@@ -0,0 +1,7 @@
+---
+"@dafunda-ui/react-drop-zone": patch
+"@dafunda-ui/next-client": patch
+"@dafunda-ui/next": patch
+---
+
+remove drop zone placeholder
--- packages/components/drop-zone/src/drop-zone.tsx
+++ packages/components/drop-zone/src/drop-zone.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 
 import { cn } from "@dafunda-ui/react-classname-utils"
@@ -14,66 +12,23 @@ export const DropZone = React.forwardRef<HTMLDivElement, DropZoneProps>(
   (props, ref) => {
     const {
       className,
-      placeholder = "Click to upload or drag and drop",
+      placeholder = "Click to upload",
       description = "JPG, JPEG, PNG or WEBP (MAX. 1280x720px)",
       ...rest
     } = props
-
-    const [isDragging, setIsDragging] = React.useState<boolean>(false)
-    const [files, setFiles] = React.useState<FileList | null>(null)
-
-    const handleDragEnter = (event: React.DragEvent<HTMLLabelElement>) => {
-      event.preventDefault()
-      setIsDragging(true)
-    }
-
-    const handleDragLeave = () => {
-      setIsDragging(false)
-    }
-
-    const handleDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
-      event.preventDefault()
-    }
-
-    const handleDrop = (event: React.DragEvent<HTMLLabelElement>) => {
-      event.preventDefault()
-      event.stopPropagation()
-      setIsDragging(false)
-      const files = event.dataTransfer.files
-      const inputElement = document.getElementById("file") as HTMLInputElement
-      if (inputElement) {
-        inputElement.files = files
-        setFiles(files)
-      }
-
-      event.dataTransfer.clearData()
-    }
 
     return (
       <div
         ref={ref}
         className={cn("flex w-full items-center justify-center", className)}
       >
-        <div
-          className={`border-border/30 bg-background/5 hover:bg-background/10 relative flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed ${
-            isDragging ? "border-primary" : ""
-          }`}
-        >
+        <div className="border-border/30 bg-background/5 hover:bg-background/10 relative flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed">
           <div className="flex flex-col items-center justify-center pb-6 pt-5">
             <Icon.UploadFile className="text-foreground/40 mb-3 h-10 w-10" />
             <p className="text-foreground/50 mb-2 text-sm">
               <span className="font-semibold">{placeholder}</span>
             </p>
             <p className="text-foreground/50 mb-2 text-xs">{description}</p>
-            {files && files?.length > 0 ? (
-              <p className="text-foreground/50 text-sm">
-                <span className="font-semibold">
-                  {files.length} files ready to upload
-                </span>
-              </p>
-            ) : (
-              ""
-            )}
           </div>
           <input
             id="file"
@@ -81,18 +36,7 @@ export const DropZone = React.forwardRef<HTMLDivElement, DropZoneProps>(
             className="hidden"
             multiple={true}
             {...rest}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setFiles(event.target.files)
-            }}
           />
-          <label
-            className="absolute inset-0"
-            onDragEnter={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            htmlFor="file"
-          ></label>
         </div>
       </div>
     )
